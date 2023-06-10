038bb8a4e7028c1e1fe94724e821d10636c962b4
dilums <dilumsanjaya@gmail.com>
2023-06-10 13:59:08 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * Simulates a coin toss and returns the result.
+ * @returns {boolean} - The result of the coin toss. `true` for "heads" and `false` for "tails".
+ */
 function coinToss() {
   return Math.random() < 0.5;
 }
